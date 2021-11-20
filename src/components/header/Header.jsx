73535a7de17edad3a14eb56c6ab91eb8338a5010
Marian-Janolek <m.janolek@gmail.com>
2021-11-20 18:27:45 +0100

    add header and map

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,36 @@
 import React from 'react';
+import { Autocomplete } from '@react-google-maps/api';
+import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+import useStyles from './styles';
 
 const Header = () => {
-  return <div>header</div>;
+  const classes = useStyles();
+  return (
+    <AppBar position="static">
+      <Toolbar className={classes.toolbar}>
+        <Typography className={classes.title} variant="h5">
+          Travel Advisor
+        </Typography>
+        <Box display="flex">
+          <Typography className={classes.title} variant="h6">
+            Explore New Places
+          </Typography>
+          {/* <Autocomplete> */}
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search ..."
+              classes={{ root: classes.inputRoot, input: classes.inputInput }}
+            />
+          </div>
+          {/* </Autocomplete> */}
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
 };
 
 export default Header;
